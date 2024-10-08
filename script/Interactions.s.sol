f3430becba9dc2e148a73bac1bdcf1e082f6a3ae
thummar05 <201260107032setice@gmail.com>
2024-10-08 17:04:40 +0530

    Initial commit for blockchain project

---
+++ script/Interactions.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+
+    string public constant LION = "https://ipfs.io/ipfs/bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4"; 
+
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("BasicNft",block.chainid);
+        mintNftOnContract(mostRecentDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(LION);
+        vm.stopBroadcast();
+    }
+}
---
+++ test/integrations/BasicNftTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../../src/BasicNft.sol";
+
+contract BasicNftTest is Test{
+    DeployBasicNft public deployer ;
+    BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant ChefSimba = "https://ipfs.io/ipfs/bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4"; 
+
+    function setUp() public{
+        deployer = new DeployBasicNft();
+        basicNft = new BasicNft();
+    }
+
+    function testNameIsCorrect() public view{
+        string memory expectedName = "Chef Simba";
+        string memory actualName = basicNft.name(); 
+
+        assert(keccak256(abi.encodePacked(expectedName))==keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(ChefSimba);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(ChefSimba)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
+    }
+}
